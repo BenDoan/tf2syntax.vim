bdac2549598963a7088ed995ddba044bfaaf9658
HorseMD <alightedness@gmail.com>
2014-05-01 20:26:56 +0100

    Update with script to download all commands from tf2 dev wiki.

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Sumant Manne for original tf2.vim syntax highlighting project.
 
 #Description
 Trying to provide basic syntax highlighting for Team Fortress 2's .cfg files.
-The current commands such as cl_interp, bind etc are all only temporarily in there
-until I write a script to scrape the list of commands off of [the tf2 commands list](https://developer.valvesoftware.com/wiki/List_of_TF2_console_commands_and_variables)
+All commands are pulled from the [TF2 Dev Wiki](https://developer.valvesoftware.com/wiki/List_of_TF2_console_commands_and_variables)
+by the ruby script update_commands.rb. Requires Nokogiri to run.
 
---
+++ update_commands.rb
@@ -0,0 +1,46 @@
+require 'nokogiri'
+require 'open-uri'
+
+class UpdateCommands
+  attr_reader :replacement_text
+
+  def initialize txt="%commands%"
+    @replacement_text = txt
+  end
+
+  # Fetch all the commands from valve's developer wiki that lists all the tf2
+  # console commands.
+  def fetch
+    url = "https://developer.valvesoftware.com/wiki/List_of_TF2_console_commands_and_variables"
+    doc = Nokogiri::HTML(open(url)).css("pre")
+    commands = []
+    for section in doc # commands are separated in <pre>'s, alphabetically. 27 in total. (first one is _)
+      for line in section.inner_text.split("\n")
+        word = line.split(" ")[0]
+        commands << word unless word.nil?
+      end
+    end
+    return commands
+  end
+
+  def create_syntax commands_list
+    data = "{{{ Commands:\n"
+    commands_list.each_slice(5) { |slice| data << "syn keyword tf2Command #{slice.join(" ")}\n"}
+    data << " }}}"
+    return data
+  end
+end
+
+if __FILE__ == $0
+  cmds = UpdateCommands.new "%commands%"
+  commands = cmds.fetch
+
+  syntax_file = File.open("#{Dir.pwd}/new_tf2.vim", "rb")
+  data = syntax_file.read
+  data.gsub!("#{cmds.replacement_text}", cmds.create_syntax(commands))
+
+  syntax_file = File.open("#{Dir.pwd}/syntax/tf2.vim", "w")
+  syntax_file.write(data)
+  syntax_file.close
+end
+
